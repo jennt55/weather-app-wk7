f65c8d0faebf4d149fa34e036dae6a965841169a
jennt55 <146074617+jennt55@users.noreply.github.com>
2023-10-03 10:17:10 +0100

    js update for city conditions

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityName = document.querySelector("#city");
+  cityName.innerHTML = response.data.city;
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = response.data.condition.description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = Math.round(response.data.temperature.humidity) + "%";
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed) + "km/h";
+}
+
 let apiKey = "331fct734c14fe46300adod3e6c6aacb";
-let apiURL = `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}`;
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}`;
+
+console.log(apiURL);
+axios.get(apiURL).then(displayTemperature);
